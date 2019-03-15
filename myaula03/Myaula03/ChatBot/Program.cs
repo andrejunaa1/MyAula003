8a4edbffd252afe5fd5b8411cbd34ba2cee60469
andrejunaa1 <cectricampeao@hotmail.com>
2019-03-15 16:50:15 +0000

    while

---
+++ myaula03/Myaula03/ChatBot/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ChatBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string respostas, perguntas;
+
+            Console.WriteLine("Qual sua questao?");
+            perguntas=Console.ReadLine();
+
+            switch(perguntas)
+            {
+                case "Onde voce mora?":
+                    respostas = "Portugal ";
+                    break;
+                case "voce esta fodido em lp?":
+                    respostas = "Com Certeza ";
+                    break;
+                case "Porque fazes video jogos":
+                    respostas = "Soy Lokito ";
+                    break;
+                default:
+                    respostas = "é analfabeto fdp? ";
+                    break;
+
+
+            }
+            Console.WriteLine(respostas);
+        }
+    }
+}
