1beb9795a3ccda685c863d2ade0d1ff84cdb23a7
andrejunaa1 <cectricampeao@hotmail.com>
2019-03-15 17:50:46 +0000

    for,and string

---
+++ myaula03/Myaula03/percorrestring/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace percorrestring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//declarar variaveis
+            string s, cstring;
+            char c;
+
+            //pedir string o user
+            Console.WriteLine("insira string:");
+            s = Console.ReadLine();
+
+            //pedir char ao user
+            Console.WriteLine("insira o char:");
+            cstring = Console.ReadLine();
+
+            //converter o char(string) em char
+            c = cstring[0];
+
+            //imprimir string
+            foreach (char cAtual in s) 
+            {
+
+                //caracter atual e diferente de c
+                if (cAtual != c)
+                {
+                    Console.Write(cAtual);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ myaula03/Myaula03/piramide/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace piramide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
