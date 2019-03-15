a2420bdc06648d235ef1f34d8027ebaabaa9d30e
andrejunaa1 <cectricampeao@hotmail.com>
2019-03-15 17:59:54 +0000

    array

---
+++ myaula03/Myaula03/arraybidim/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace arraybidim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         //declarar variaveis
+         //pediir as linhas e converter p int
+         //"colunas"
+         //pedir valores p matriz--for(for)
+         //imprimir matriz de forma formatada
+         //calcukar as medias das linhas
+         //somar as medias
+         //imprimir as medias 
+         //imprimir as somas das medias
+        }
+    }
+}
