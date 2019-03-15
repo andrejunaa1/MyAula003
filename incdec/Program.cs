d33bdc2388e743cd17996034c9cc7bdb059b868f
andrejunaa1 <cectricampeao@hotmail.com>
2019-03-15 14:41:55 +0000

    operacoes ++ --

---
+++ incdec/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace incdec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 5;
+            int c = ++a + b++;
+            int d = --c - b--;
+
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+        }
+    }
+}
